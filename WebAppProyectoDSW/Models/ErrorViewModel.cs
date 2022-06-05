1ac41d72b6e5df8e92b92ed7fff7300204daa5b3
JoseAntonioZero <joseantoniovc.729@gmail.com>
2022-06-05 18:24:26 -0500

    Agregar archivos de proyecto.

---
+++ WebAppProyectoDSW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppProyectoDSW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
